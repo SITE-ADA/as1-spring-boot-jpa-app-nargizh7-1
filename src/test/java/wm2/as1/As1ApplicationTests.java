385d9891cce3b239be9ebeabbb231465c1930529
Nargiz Heybatova <nargizheybatova07@gmail.com>
2024-03-13 21:47:43 +0400

    new project, switched to gradle

---
+++ src/test/java/wm2/as1/As1ApplicationTests.java
@@ -0,0 +1,13 @@
+package wm2.as1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class As1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
