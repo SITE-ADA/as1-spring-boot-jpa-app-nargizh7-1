35a4f4b4899bc690eb5d759bf9b1e2030307fe11
Nargiz Heybatova <nargizheybatova07@gmail.com>
2024-03-15 16:26:26 +0400

    deleted all project files due to version inconsistancies

---
+++ src/test/java/wm2/as1/AS1/As1ApplicationTests.java
@@ -0,0 +1,13 @@
+package wm2.AS1.AS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class As1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
