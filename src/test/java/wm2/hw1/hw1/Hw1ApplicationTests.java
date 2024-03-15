819900e3cd36363a878202784cd5daa3fbc168f2
Nargiz Heybatova <nargizheybatova07@gmail.com>
2024-03-15 16:43:18 +0400

    gradle prints the message

---
+++ src/test/java/wm2/hw1/hw1/Hw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package wm2.hw1.hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
