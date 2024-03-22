0dba0bbd6e950012f5ef218393f7cc9fd07f6c63
Nargiz Heybatova <nargizheybatova07@gmail.com>
2024-03-22 20:40:51 +0400

    project finished

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,23 +4,22 @@ INSERT INTO DIRECTORS(first_name, last_name) VALUES('Steven', 'Spielberg');
 INSERT INTO DIRECTORS(first_name, last_name) VALUES('Martin', 'Scorsese');
 INSERT INTO DIRECTORS(first_name, last_name) VALUES('Alfred', 'Hitchcock');
 
-INSERT INTO MOVIES(name, country, wins) VALUES('Inception', 'USA', 8.8);
-INSERT INTO MOVIES(name, country, wins) VALUES('The Dark Knight', 'USA', 9.0);
-INSERT INTO MOVIES(name, country, wins) VALUES('Interstellar', 'USA', 8.6);
+INSERT INTO MOVIES(name, country, wins) VALUES('Inception', 'USA', 4);
+INSERT INTO MOVIES(name, country, wins) VALUES('The Dark Knight', 'USA', 2);
+INSERT INTO MOVIES(name, country, wins) VALUES('Interstellar', 'USA', 1);
 
-INSERT INTO MOVIES(name, country, wins) VALUES('Pulp Fiction', 'USA', 8.9);
-INSERT INTO MOVIES(name, country, wins) VALUES('Django Unchained', 'USA', 8.4);
+INSERT INTO MOVIES(name, country, wins) VALUES('Pulp Fiction', 'USA', 1);
+INSERT INTO MOVIES(name, country, wins) VALUES('Django Unchained', 'USA', 2);
 
-INSERT INTO MOVIES(name, country, wins) VALUES('Jurassic Park', 'USA', 8.2);
-INSERT INTO MOVIES(name, country, wins) VALUES('Schindlers List', 'USA', 9.0);
+INSERT INTO MOVIES(name, country, wins) VALUES('Jurassic Park', 'USA', 3);
+INSERT INTO MOVIES(name, country, wins) VALUES('Schindlers List', 'USA', 7);
 
-INSERT INTO MOVIES(name, country, wins) VALUES('The Departed', 'USA', 8.5);
-INSERT INTO MOVIES(name, country, wins) VALUES('Goodfellas', 'USA', 8.7);
+INSERT INTO MOVIES(name, country, wins) VALUES('The Departed', 'USA', 4);
+INSERT INTO MOVIES(name, country, wins) VALUES('Goodfellas', 'USA', 1);
 
-INSERT INTO MOVIES(name, country, wins) VALUES('Psycho', 'USA', 8.5);
-INSERT INTO MOVIES(name, country, wins) VALUES('Rear Window', 'USA', 8.5);
+INSERT INTO MOVIES(name, country, wins) VALUES('Psycho', 'USA', 0);
+INSERT INTO MOVIES(name, country, wins) VALUES('Rear Window', 'USA', 0);
 
--- Mapping directors to movies
 INSERT INTO director_movie VALUES(1, 1);
 INSERT INTO director_movie VALUES(1, 2);
 INSERT INTO director_movie VALUES(1, 3);
