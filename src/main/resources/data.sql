dec001663c4bd5c18895880811439f37c6e6d9ac
Nargiz Heybatova <nargizheybatova07@gmail.com>
2024-03-22 20:30:46 +0400

    added several movies to the list for  each director

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,3 @@
---INSERT INTO STUDENTS(first_name, last_name) VALUES('Sabrina-Maria', 'Sakka')
---INSERT INTO STUDENTS(first_name, last_name) VALUES('Ilaha', 'Behbudova')
---INSERT INTO STUDENTS(first_name, last_name) VALUES('Nuraddin', 'Sadili')
---INSERT INTO STUDENTS(first_name, last_name) VALUES('Emil', 'Huseynov')
---INSERT INTO STUDENTS(first_name, last_name) VALUES('Huseyn', 'Jabbarov')
---INSERT INTO STUDENTS(first_name, last_name) VALUES('Kanan', 'Gafarov')
-
--- to avoid duplicate of values (data folder)
---DELETE from MOVIES;
-
---INSERT INTO MOVIES(name, description, credits) VALUES('Inception', 'Sci-fi thriller', 6)
---INSERT INTO MOVIES(name, description, credits) VALUES('The Matrix', 'Cyberpunk action', 6)
---INSERT INTO MOVIES(name, description, credits) VALUES('Interstellar', 'Space exploration', 6)
---INSERT INTO MOVIES(name, description, credits) VALUES('The Dark Knight', 'Superhero drama', 6)
---INSERT INTO MOVIES(name, description, credits) VALUES('Avatar', 'Epic sci-fi', 6)
-
---INSERT INTO student_course VALUES(2, 2)
---INSERT INTO student_course VALUES(2, 5)
---INSERT INTO student_course VALUES(1, 1)
---INSERT INTO student_course VALUES(1, 3)
---INSERT INTO student_course VALUES(1, 4)
---INSERT INTO student_course VALUES(4, 2)
---INSERT INTO student_course VALUES(4, 3)
---INSERT INTO student_course VALUES(4, 5)
-
 INSERT INTO DIRECTORS(first_name, last_name) VALUES('Christopher', 'Nolan');
 INSERT INTO DIRECTORS(first_name, last_name) VALUES('Quentin', 'Tarantino');
 INSERT INTO DIRECTORS(first_name, last_name) VALUES('Steven', 'Spielberg');
@@ -30,13 +5,34 @@ INSERT INTO DIRECTORS(first_name, last_name) VALUES('Martin', 'Scorsese');
 INSERT INTO DIRECTORS(first_name, last_name) VALUES('Alfred', 'Hitchcock');
 
 INSERT INTO MOVIES(name, country, wins) VALUES('Inception', 'USA', 8.8);
-INSERT INTO MOVIES(name, country, wins) VALUES('Pulp Fiction', 'USA', 8.9);
-INSERT INTO MOVIES(name, country, wins) VALUES('Jurassic Park', 'USA', 8.2);
-INSERT INTO MOVIES(name, country, wins) VALUES('The Departed', 'USA', 8.5);
-INSERT INTO MOVIES(name, country, wins) VALUES('Psycho', 'USA', 8.5);
+INSERT INTO MOVIES(name, country, wins) VALUES('The Dark Knight', 'USA', 9.0);
+INSERT INTO MOVIES(name, country, wins) VALUES('Interstellar', 'USA', 8.6);
 
+INSERT INTO MOVIES(name, country, wins) VALUES('Pulp Fiction', 'USA', 8.9);
+INSERT INTO MOVIES(name, country, wins) VALUES('Django Unchained', 'USA', 8.4);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('Jurassic Park', 'USA', 8.2);
+INSERT INTO MOVIES(name, country, wins) VALUES('Schindlers List', 'USA', 9.0);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('The Departed', 'USA', 8.5);
+INSERT INTO MOVIES(name, country, wins) VALUES('Goodfellas', 'USA', 8.7);
+
+INSERT INTO MOVIES(name, country, wins) VALUES('Psycho', 'USA', 8.5);
+INSERT INTO MOVIES(name, country, wins) VALUES('Rear Window', 'USA', 8.5);
+
+-- Mapping directors to movies
 INSERT INTO director_movie VALUES(1, 1);
-INSERT INTO director_movie VALUES(2, 2);
-INSERT INTO director_movie VALUES(3, 3);
-INSERT INTO director_movie VALUES(4, 4);
-INSERT INTO director_movie VALUES(5, 5);
+INSERT INTO director_movie VALUES(1, 2);
+INSERT INTO director_movie VALUES(1, 3);
+
+INSERT INTO director_movie VALUES(2, 4);
+INSERT INTO director_movie VALUES(2, 5);
+
+INSERT INTO director_movie VALUES(3, 6);
+INSERT INTO director_movie VALUES(3, 7);
+
+INSERT INTO director_movie VALUES(4, 8);
+INSERT INTO director_movie VALUES(4, 9);
+
+INSERT INTO director_movie VALUES(5, 10);
+INSERT INTO director_movie VALUES(5, 11);
